95531fb202ab2b6402b785baf255581f223513a9
rayhans358 <rayhans358@gmail.com>
2023-04-03 23:08:04 +0700

    Eduwork_Tugas3

---
+++ index.js
@@ -0,0 +1,17 @@
+let inputNilai = prompt(`Silahkan masukkan nilai anda : `);
+
+if (!inputNilai) {
+  alert (`Silahkan masukkan nilai anda terlebih dahulu`);
+} else if (inputNilai > 100) {
+  alert (`Silahkan masukkan nilai yang benar`);
+} else if (inputNilai >= 80 && inputNilai <= 100) {
+  alert(`Selamat! Nilai anda A`);
+} else if (inputNilai >= 60 && inputNilai < 80) {
+  alert(`Selamat! Nilai anda B`);
+} else if (inputNilai >= 40 && inputNilai < 60) {
+  alert(`Selamat! Nilai anda C`);
+} else if (inputNilai >= 20 && inputNilai < 60) {
+  alert(`Selamat! Nilai anda D`);
+} else {
+  alert(`Selamat! Nilai anda E`);
+}
